c67ec377c7f3ec57317009f474d0f7c2759b3646
arg4 <nategrimberg@gmail.com>
2017-12-10 22:36:37 -0500

    added an initial unitest

---
+++ project/tests/test.py
@@ -0,0 +1,30 @@
+import unittest
+
+from project import app
+
+
+class ProjectTests(unittest.TestCase):
+
+    # Setup and teardown
+
+    # executed prior to each test
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+
+        self.assertEquals(app.debug, False)
+
+    # executed after each test
+    def tearDown(self):
+        pass
+
+    # Helper Methods
+
+    # Tests
+    def test_main_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertIn(b'Employee Activity Registration', response.data)
+
+if __name__ == "__main__":
+    unittest.main()
